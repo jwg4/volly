50ad3203af81d59a839a7fb6b74784fabe4a389f
Jack Grahl <jack.grahl@gmail.com>
2019-11-17 16:52:25 +0000

    Doctest fails for missing package.

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,3 +7,8 @@ class TestService(TestCase):
     def test_init(self):
         svc = Service("http://www.example.com")
         self.assertIsNotNone(svc)
+
+    def test_read_and_write(self):
+        svc = Service("http://www.example.com")
+        svc["ZFHIAUNGYI"] = "P3XD8NWG7K"
+        self.assertEqual(svc["ZFHIAUNGYI"], "P3XD8NWG7K")
