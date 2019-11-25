f34baaffa7c9e3eba9cf558f4b17b7f7ebea4978
Jack Grahl <jack.grahl@gmail.com>
2019-11-25 21:45:28 +0000

    Add tests using the substitute service.

---
+++ tests/test_service.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from volly import Service
+from volly.error import MissingKeyException
+
+
+class TestService(TestCase):
+    def test_init(self):
+        svc = Service("http://www.example.com")
+        self.assertIsNotNone(svc)
+
+    def test_read_and_write(self):
+        svc = Service("https://volatile.wtf")
+        svc["ZFHIAUNGYI"] = "P3XD8NWG7K"
+        self.assertEqual(svc["ZFHIAUNGYI"], b'P3XD8NWG7K')
+
+    def test_write_missing_value(self):
+        svc = Service("https://volatile.wtf")
+        self.assertRaises(MissingKeyException, lambda: svc["UNGYIZFHIA"])
---
+++ tests/test_substitute.py
@@ -0,0 +1,16 @@
+from volly.testing import TestService
+
+
+class TestSubstituteService(TestCase):
+    def test_init(self):
+        svc = TestService("http://www.example.com")
+        self.assertIsNotNone(svc)
+
+    def test_read_and_write(self):
+        svc = TestService("https://volatile.wtf")
+        svc["ZFHIAUNGYI"] = "P3XD8NWG7K"
+        self.assertEqual(svc["ZFHIAUNGYI"], b'P3XD8NWG7K')
+
+    def test_write_missing_value(self):
+        svc = TestService("https://volatile.wtf")
+        self.assertRaises(MissingKeyException, lambda: svc["UNGYIZFHIA"])
