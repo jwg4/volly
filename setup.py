1af8e78d8e35dc0b092108fefedbffad1e9271c5
Jack Grahl <jack.grahl@gmail.com>
2019-11-27 22:47:37 +0000

    Add a description.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='volly',
     version='0.1.0',
     description='A Python client for volatile.wtf',
+    long_description='Volatile - https://volatilie.wtf is a public service which provides a key-value pair API. This package provides a convenient Python wrapper for that service.',
     url='http://github.com/jwg4/volly',
     author='Jack Grahl',
     author_email='jack.grahl@gmail.com',
